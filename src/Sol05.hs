f510c58070f0c22f56f34c5ef1e6f85ccf1442ef
Dave Aitken <dave.aitken@gmail.com>
2017-12-07 23:29:12 +0000

    Day 5 part 2 with Data.Sequence

--- src/Sol05.hs
+++ src/Sol05.hs
@@ -4,20 +4,25 @@ module Sol05
 
 import Data.Sequence as Seq
 
-runPart1 :: Int -> Int -> Seq Int -> Int
-runPart1 c i xs = if i >= Seq.length xs
+calcSteps :: (Int -> Int) -> Int -> Int -> Seq Int -> Int
+calcSteps f c i xs = if i >= Seq.length xs
   then c
   else let j = Seq.index xs i
-       in runPart1 (c+1) (i+j) (Seq.update i (j+1) xs)
+       in calcSteps f (c+1) (i+j) (Seq.update i (f j) xs)
 
 parseInput f = Seq.fromList $ do
   l <- lines f
   return $ (read l :: Int)
 
 part1 :: Seq Int -> Int
-part1 = runPart1 0 0
+part1 = calcSteps ((+) 1) 0 0
+
+part2 :: Seq Int -> Int
+part2 = calcSteps (\j -> if j >= 3 then j-1 else j+1) 0 0
 
 sol05 :: IO ()
 sol05 = do
   input <- readFile "data/05.txt"
   putStrLn (show . part1 . parseInput $ input)
+  putStrLn (show . part2 . parseInput $ input)
+
