e0c43638775fc6b0c86a90902424537c4e8bf160
Dave Aitken <dave.aitken@gmail.com>
2017-12-09 17:08:11 +0000

    Day 7 part 1

--- src/Sol07.hs
+++ src/Sol07.hs
@@ -4,6 +4,7 @@ module Sol07
 
 import Text.Parsec
 import Text.Parsec.String
+import qualified Data.Set as S
 
 data Mapping =
   Mapping { name :: String, weight :: Int, supports :: [String] }
@@ -32,8 +33,15 @@ mappingParser = do
 parseInput :: String -> String -> Either ParseError [Mapping]
 parseInput fname input = parse (many mappingParser) fname input
 
+nameOfRoot :: [Mapping] -> String
+nameOfRoot ms = let
+  names = S.fromList $ map name ms
+  refs = S.fromList $ concatMap supports ms
+  in
+  head $ S.toList (S.difference names refs)
+
 sol07 :: IO ()
 sol07 = let fname = "data/07.txt" in do
   input <- readFile fname
-  putStrLn (show (fmap last (parseInput fname input)))
+  putStrLn (show (fmap nameOfRoot (parseInput fname input)))
 
