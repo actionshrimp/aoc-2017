f7d07ab605962debcc4d2f85e722aff099309001
Dave Aitken <dave.aitken@gmail.com>
2017-12-11 21:04:13 +0000

    Day 9 part 2

--- src/Sol09.hs
+++ src/Sol09.hs
@@ -8,15 +8,16 @@ data ExecSt
            , score :: Int
            , cancelNext :: Bool
            , garbage :: Bool
+           , gCount :: Int
            }
   deriving Show
 
 parseStream :: ExecSt -> Char -> ExecSt
 parseStream s@(ExecSt { cancelNext = False }) '!' = s{ cancelNext = True }
 parseStream s@(ExecSt { cancelNext = True }) _ = s{ cancelNext = False }
-parseStream s@(ExecSt { cancelNext = False, garbage }) '<' = s{ garbage = True }
+parseStream s@(ExecSt { cancelNext = False, garbage = False }) '<' = s{ garbage = True }
 parseStream s@(ExecSt { cancelNext = False, garbage = True }) '>' = s{ garbage = False }
-parseStream s@(ExecSt { cancelNext = False, garbage = True })  _ = s
+parseStream s@(ExecSt { cancelNext = False, garbage = True, gCount })  _ = s{ gCount = gCount + 1 }
 parseStream s@(ExecSt { depth, score }) '{' = s{ depth = depth + 1 }
 parseStream s@(ExecSt { depth, score }) '}' = s{ depth = depth - 1, score = score + depth }
 parseStream s  _ = s
@@ -24,9 +25,17 @@ parseStream s  _ = s
 initState = ExecSt { depth = 0
                     , score = 0
                     , cancelNext = False
-                    , garbage = False }
+                    , garbage = False
+                    , gCount = 0
+                    }
+
+runParse :: String -> ExecSt
+runParse s =
+  foldl parseStream initState s
 
 run :: IO ()
 run = let fname = "data/09.txt" in do
   input <- readFile fname
-  putStrLn (show (foldl parseStream initState input))
+  putStrLn (show (runParse input))
+  putStrLn $ "part 1: " ++ (show (score (runParse input)))
+  putStrLn $ "part 2: " ++ (show (gCount (runParse input)))
