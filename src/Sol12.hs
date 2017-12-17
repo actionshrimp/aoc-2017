37aa05ea332aed30f3f4ab1006e88c4599b6eedd
Dave Aitken <dave.aitken@gmail.com>
2017-12-17 23:43:39 +0000

    Day 12 part 2

--- src/Sol12.hs
+++ src/Sol12.hs
@@ -14,22 +14,27 @@ parseInput input = do
     in
     map read $ splitOn ", " r
 
-workPart1 :: [[Int]] -> S.Set Int -> [Int]-> S.Set Int
-workPart1 input visited [] = visited
-workPart1 input visited xs = let
+visitAll :: [[Int]] -> S.Set Int -> [Int]-> S.Set Int
+visitAll input visited [] = visited
+visitAll input visited xs = let
   children = concatMap (\x -> input !! x) xs
   new = filter (\x -> S.notMember x visited) children
   in
-  workPart1 input (S.union visited (S.fromList new)) new
+  visitAll input (S.union visited (S.fromList new)) new
+
+visitedFrom :: [[Int]] -> Int -> S.Set Int
+visitedFrom input start = visitAll input (S.fromList [start]) [start]
 
 part1 :: [[Int]] -> Int
-part1 input = let
-  visited = workPart1 input (S.fromList [0]) [0]
-  in
-  S.size visited
+part1 input = S.size $ visitedFrom input 0
+
+-- Repeats the work but nice and clear
+part2 :: [[Int]] -> Int
+part2 input = S.size $ foldl (\s x -> S.union s (S.singleton $ visitedFrom input x)) S.empty [0..(length input - 1)]
 
 run :: IO ()
 run = do
   rawInput <- readFile "data/12.txt"
   let input = parseInput rawInput
   putStrLn $ "part1: " ++ (show (part1 input))
+  putStrLn $ "part2: " ++ (show (part2 input))
