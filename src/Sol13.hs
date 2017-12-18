98cdb8c4e77b5bff347ac1f6a7460af08a6637f9
Dave Aitken <dave.aitken@gmail.com>
2017-12-18 10:45:25 +0000

    Day 13 with much less ceremony now I know what part 2 is

--- src/Sol13.hs
+++ src/Sol13.hs
@@ -5,6 +5,7 @@ module Sol13
 
 import Data.List (elemIndex)
 import Data.List.Split (splitOn)
+import Data.Maybe (catMaybes)
 import qualified Data.Set as S
 
 parseInput :: String -> [(Int, Int)]
@@ -12,51 +13,28 @@ parseInput input = do
   l <- lines input
   return $ let [a, b] = splitOn ": " l in (read a, read b)
 
-data Layer = Scanner { y :: Int, range :: Int, direction :: Int } | NoScanner
-  deriving Show
-
-initState :: [(Int, Int)] -> [Layer]
-initState input = go 0 input where
-    go i [] = []
-    go i xs@((j, r):ys) =
-      (if i == j
-      then [Scanner { range = r, direction = 1, y = 0 }] ++ go (i+1) ys
-      else [NoScanner] ++ go (i+1) xs)
-
-stepLayer :: Layer -> Layer
-stepLayer NoScanner = NoScanner
-stepLayer s@Scanner{y, range, direction} =
-  if y + direction < 0 || y + direction > (range - 1)
-  then s { y = y - direction, direction = -direction}
-  else s { y = y + direction }
-
-stepFirewall :: [Layer] -> [Layer]
-stepFirewall fw = map stepLayer fw
-
-walkWall :: (Int, Int) -> [Layer] -> [Int]
-walkWall _ [] = []
-walkWall (wx,wy) (l:ls) =
-  (case l of
-    NoScanner -> []
-    s@Scanner{y, range} -> if wy == y then [wx * range] else [])
-  ++ walkWall (wx+1, wy) (stepFirewall ls)
-
 exampleInput :: [(Int, Int)]
 exampleInput = [(0, 3), (1, 2), (4, 4), (6, 4)]
 
-part1BruteForce :: [(Int, Int)] -> Int
-part1BruteForce input = let
-  is = initState input
-  in foldl (+) 0 $ walkWall (0, 0) is
+found :: Int -> (Int, Int) -> Maybe Int
+found delay (depth, range) =
+  if (depth + delay) `rem` (2 * (range - 1)) == 0
+  then Just (depth * range)
+  else Nothing
 
-part2BruteForce :: [(Int, Int)] -> Maybe Int
-part2BruteForce input = let
-  is = initState input
-  in elemIndex [] $ map (walkWall (0, 0)) (iterate stepFirewall is)
+severities :: [(Int, Int)] -> Int -> [Int]
+severities input delay =
+  catMaybes $ map (found delay) input
+
+part1 :: [(Int, Int)] -> Int
+part1 input = foldl (+) 0 $ severities input 0
+
+part2Brute :: [(Int, Int)] -> Maybe Int
+part2Brute input = elemIndex [] $ map (severities input) [0..]
 
 run :: IO ()
 run = do
   rawInput <- readFile "data/13.txt"
   let input = parseInput rawInput
-  putStrLn $ "part1: " ++ (show (part1BruteForce input))
-  putStrLn $ "part2: " ++ (show (part2BruteForce input))
+  putStrLn $ "part1: " ++ (show (part1 input))
+  putStrLn $ "part2: " ++ (show (part2Brute input))
