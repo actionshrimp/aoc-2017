496e4c4b8ac7d513d7581c4f3f10ae0cdcbf410b
Dave Aitken <dave.aitken@gmail.com>
2017-12-18 11:17:47 +0000

    Day 13 part 2 without extra work when already found (2x faster)

--- src/Sol13.hs
+++ src/Sol13.hs
@@ -16,15 +16,15 @@ parseInput input = do
 exampleInput :: [(Int, Int)]
 exampleInput = [(0, 3), (1, 2), (4, 4), (6, 4)]
 
-found :: Int -> (Int, Int) -> Maybe Int
-found delay (depth, range) =
+severity :: Int -> (Int, Int) -> Maybe Int
+severity delay (depth, range) =
   if (depth + delay) `rem` (2 * (range - 1)) == 0
   then Just (depth * range)
   else Nothing
 
 severities :: [(Int, Int)] -> Int -> [Int]
 severities input delay =
-  catMaybes $ map (found delay) input
+  catMaybes $ map (severity delay) input
 
 part1 :: [(Int, Int)] -> Int
 part1 input = foldl (+) 0 $ severities input 0
@@ -32,9 +32,19 @@ part1 input = foldl (+) 0 $ severities input 0
 part2Brute :: [(Int, Int)] -> Maybe Int
 part2Brute input = elemIndex [] $ map (severities input) [0..]
 
+found :: [(Int, Int)] -> Int -> Bool
+found [] delay = False
+found (x@(depth, range):xs) delay =
+  if (depth + delay) `rem` (2 * (range - 1)) == 0
+  then True
+  else found xs delay
+
+part2Faster :: [(Int, Int)] -> Int
+part2Faster input = head $ filter (not . found input) [0..]
+
 run :: IO ()
 run = do
   rawInput <- readFile "data/13.txt"
   let input = parseInput rawInput
   putStrLn $ "part1: " ++ (show (part1 input))
-  putStrLn $ "part2: " ++ (show (part2Brute input))
+  putStrLn $ "part2: " ++ (show (part2Faster input))
