b2a700643b4c035d8018cc48369f59a16477de64
Dave Aitken <dave.aitken@gmail.com>
2017-12-23 14:38:39 +0000

    Day 16 pt 2 with an index

--- src/Sol16.hs
+++ src/Sol16.hs
@@ -4,43 +4,58 @@ module Sol16
     ) where
 
 import qualified Data.Sequence as Sq
+import qualified Data.Map.Strict as M
 import Data.Foldable (toList)
 import Data.Maybe (fromMaybe)
 import Data.List.Split (splitOn)
 
 data DanceStep = Spin Int | Exchange Int Int | Partner Char Char
-type Line = Sq.Seq Char
+data Line = Line
+  { lSeq :: Sq.Seq Char
+  , lIdx :: M.Map Char Int
+  }
+
+spin :: Line -> Int -> Line
+spin (Line {lSeq, lIdx}) i = let
+  len = length lSeq
+  newList = take len $ drop (len - i) $ (cycle (toList lSeq))
+  in makeLine newList
 
 exchange :: Line -> Int -> Int -> Line
-exchange line i j = let
-  atI = Sq.index line i
-  atJ = Sq.index line j
-  in Sq.adjust (const atI) j . Sq.adjust (const atJ) i $ line
+exchange (Line {lSeq, lIdx}) i j = let
+  atI = Sq.index lSeq i
+  atJ = Sq.index lSeq j
+  in Line { lSeq = Sq.adjust (const atI) j . Sq.adjust (const atJ) i $ lSeq
+          , lIdx = M.adjust (const j) atI . M.adjust (const i) atJ $ lIdx
+          }
 
 partner :: Line -> Char -> Char -> Line
-partner line a b = fromMaybe line $ do
-  idxA <- Sq.elemIndexL a line
-  idxB <- Sq.elemIndexL b line
+partner line@(Line {lSeq, lIdx}) a b = fromMaybe line $ do
+  idxA <- M.lookup a lIdx
+  idxB <- M.lookup b lIdx
   return $ exchange line idxA idxB
 
 danceStep :: Line -> DanceStep -> Line
-danceStep line s =
-  let l = length line in
-    case s of
-      Spin i -> Sq.fromList $ take l $ drop (l - i) $ (cycle (toList line))
-      Exchange i j -> exchange line i j
-      Partner a b -> partner line a b
+danceStep line (Spin i) = spin line i
+danceStep line (Exchange i j) = exchange line i j
+danceStep line (Partner a b) = partner line a b
 
-exampleLine = Sq.fromList "abcde"
-exampleInstrs = [Spin 1, Exchange 3 4, Partner 'e' 'b']
+makeLine s =
+  Line { lSeq = Sq.fromList s
+       , lIdx = M.fromList $ zip s [0..]
+       }
 
-example :: Line
-example = foldl danceStep exampleLine exampleInstrs
+danceResult :: String -> [DanceStep] -> String
+danceResult l steps =
+  toList . lSeq $ foldl danceStep (makeLine l) steps
 
-line = Sq.fromList "abcdefghijklmnop"
+exampleSteps = [Partner 'a' 'b', Exchange 0 1]
 
-part1 :: [DanceStep] -> Line
-part1 steps = foldl danceStep line steps
+example :: String
+example = danceResult "abcde" exampleSteps
+
+part1 :: [DanceStep] -> String
+part1 steps = danceResult "abcdefghijklmnop" steps
 
 parseStep :: String -> DanceStep
 parseStep ('s' : xs) = Spin (read xs)
@@ -55,4 +70,6 @@ parseInput s = do
 run :: IO ()
 run = do
   input <- readFile "data/16.txt"
-  putStrLn $ "part1: " ++ (show (part1 $ parseInput input))
+  let dance = parseInput input
+  putStrLn $ "part1: " ++ (show (part1 dance))
+  putStrLn $ "part2: " ++ (show (part1 (concat (take 1000000000 (repeat dance)))))
