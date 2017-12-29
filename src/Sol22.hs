f041b1b6197fb5b65f7869401a44fd645f22ca71
Dave Aitken <dave.aitken@gmail.com>
2017-12-29 14:35:29 +0000

    Day 22 part 1 with different states

--- src/Sol22.hs
+++ src/Sol22.hs
@@ -9,12 +9,14 @@ import Data.Maybe (catMaybes, fromMaybe)
 data Carrier = Carrier { dir :: (Int, Int), pos :: (Int, Int) }
   deriving Show
 
-type Infected = M.Map (Int, Int) Bool
+data State = Weakened | Infected | Flagged
 
-seed :: String -> (Int,Carrier, Infected)
+type States = M.Map (Int, Int) State
+
+seed :: String -> (Int, Carrier, States)
 seed s = let
   grid = lines s
-  mapped = zipWith (\y row -> zipWith (\x c -> if c == '#' then Just ((x, y), True) else Nothing) [0..] row) [0..] $ grid
+  mapped = zipWith (\y row -> zipWith (\x c -> if c == '#' then Just ((x, y), Infected) else Nothing) [0..] row) [0..] $ grid
   (x, y) = ((length (head grid)) `div` 2, length grid `div` 2)
   in (0, Carrier { pos = (x, y), dir = (0, -1)}, M.fromList . catMaybes . concat $ mapped)
 
@@ -34,23 +36,29 @@ turnLeft ( 0, -1) = (-1,  0)
 turnLeft ( 1,  0) = ( 0, -1)
 turnLeft (-1,  0) = ( 0,  1)
 
-step :: (Int, Carrier, Infected) -> (Int, Carrier, Infected)
-step (x, c@Carrier {dir, pos = pos@(px, py)}, inf) = let
-  infected = M.findWithDefault False pos inf
-  dir'@(d'x, d'y) = if infected then turnRight dir else turnLeft dir
-  inf' = if infected then M.delete pos inf else M.insert pos True inf
-  x' = if infected then x else x + 1
+stepPart1 :: (Int, Carrier, States) -> (Int, Carrier, States)
+stepPart1 (x, c@Carrier {dir, pos = pos@(px, py)}, inf) = let
+  state = M.lookup pos inf
+  dir'@(d'x, d'y) = case state of
+    Just Infected -> turnRight dir
+    Nothing -> turnLeft dir
+  inf' = case state of
+    Just Infected -> M.delete pos inf
+    Nothing -> M.insert pos Infected inf
+  x' = case state of
+    Just Infected -> x
+    Nothing -> x + 1
   in (x', c {dir = dir', pos = (px + d'x, py + d'y)}, inf')
 
 exampleSeed = seed $ "..#\n#..\n..."
 
-causedInfection seed steps = let
-  (x, c, inf) = head . drop steps $ iterate step seed
+causedInfection stepper seed steps = let
+  (x, c, inf) = head . drop steps $ iterate stepper seed
   in x
 
-example = causedInfection exampleSeed 10000
+examplePart1 = causedInfection stepPart1 exampleSeed 10000
 
 run :: IO ()
 run = do
   seedRaw <- readFile "data/22.txt"
-  putStrLn $ "part1: " ++ (show (causedInfection (seed seedRaw) 10000))
+  putStrLn $ "part1: " ++ (show (causedInfection stepPart1 (seed seedRaw) 10000))
