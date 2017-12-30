db73e46bed51dfa751478567a624e2a488ca087e
Dave Aitken <dave.aitken@gmail.com>
2017-12-30 15:26:05 +0000

    Day 24 part 2 - switching to seqs shaves another 20% or so off

--- src/Sol24.hs
+++ src/Sol24.hs
@@ -7,30 +7,50 @@ import Data.List.Split (splitOn)
 import Data.List (delete, nub, sort, sortBy)
 import Data.Foldable (foldl')
 import Data.Ord (comparing)
+import Data.Maybe (fromMaybe, fromJust)
 import qualified Data.Sequence as Seq
 
+seqInit :: Seq.Seq a -> Seq.Seq a
+seqInit xs = let l = Seq.length xs in Seq.deleteAt (l-1) xs
+
+seqMap :: (a -> b) -> Seq.Seq a -> Seq.Seq b
+seqMap f xs = Seq.mapWithIndex (\i x -> f x) xs
+
+seqHead :: Seq.Seq a -> a
+seqHead = fromJust . (Seq.lookup 0)
+
+seqFoldl :: (b -> a -> b) -> b -> Seq.Seq a -> b
+seqFoldl f = Seq.foldlWithIndex (\acc i x -> f acc x)
+
+seqDelete :: Eq a => a -> Seq.Seq a -> Seq.Seq a
+seqDelete x xs = fromMaybe xs $ do
+  i <- Seq.elemIndexL x xs
+  return $ Seq.deleteAt i xs
+
 --Components
-type Cs = [(Int, Int)]
+type C = (Int, Int)
+type Cs = Seq.Seq C
 
 parseInput :: String -> Cs
-parseInput s = [(read a, read b) | l  <- lines s, let [a, b] = splitOn "/" l ]
+parseInput s = Seq.fromList [(read a, read b) | l  <- lines s, let [a, b] = splitOn "/" l ]
 
-expandOne :: (Cs, Cs) -> Maybe [(Cs, Cs)]
-expandOne (available, current@((_, cp):cs)) =
-  case filter (\(apa, apb) -> apa == cp || apb == cp) available of
-    [] -> Nothing
-    matches -> Just $ map (\a@(apa, apb) -> (delete a available, if apa == cp then a:current else (apb, apa):current)) matches
+expandOne :: (Cs, Cs) -> Maybe (Seq.Seq (Cs, Cs))
+expandOne (available, current) = let
+  (_, cp) = seqHead current
+  matches = Seq.filter (\(apa, apb) -> apa == cp || apb == cp) available in
+  if | matches == Seq.empty -> Nothing
+     | otherwise -> Just $ seqMap (\a@(apa, apb) -> (seqDelete a available, if apa == cp then a Seq.<| current else (apb, apa) Seq.<| current)) matches
 
-expandMany :: [(Cs, Cs)] -> [(Cs, Cs)]
-expandMany work = foldl' f [] work where
+expandMany :: Seq.Seq (Cs, Cs) -> Seq.Seq (Cs, Cs)
+expandMany work = seqFoldl f Seq.empty work where
   f moreWork w = case expandOne w of
     Nothing -> moreWork
-    Just exps -> exps ++ moreWork
+    Just exps -> exps Seq.>< moreWork
 
-validBridges' :: Cs -> [Cs]
-validBridges' cs = delete [] $ go [(cs, [(0, 0)])] where
-  go [] = []
-  go work = map (init . snd) work ++ go (expandMany work)
+validBridges' :: Cs -> Seq.Seq (Cs)
+validBridges' cs = Seq.drop 1 $ go (Seq.singleton (cs, Seq.singleton (0, 0))) where
+  go work | work == Seq.empty = Seq.empty
+          | otherwise = seqMap (seqInit . snd) work Seq.>< go (expandMany work)
 
 exampleComponentsRaw = "\
 \0/2\n\
@@ -42,17 +62,19 @@ exampleComponentsRaw = "\
 \10/1\n\
 \9/10"
 
+ecs = parseInput exampleComponentsRaw
+
 strength :: Cs -> Int
-strength cs = sum $ map (\(a, b) -> a+b) cs
+strength cs = sum $ seqMap (\(a, b) -> a+b) cs
 
-strongest :: [Cs] -> Int
-strongest bs = foldl' (\acc x -> max (strength x) acc) 0 bs
+strongest :: Seq.Seq Cs -> Int
+strongest bs = seqFoldl (\acc x -> max (strength x) acc) 0 bs
 
-longest :: [Cs] -> [Cs]
-longest bs = foldl' f [[]] bs where
-  f acc@(a:_) x | length x > length a = [x]
-                | length x == length a = x:acc
-                | otherwise = acc
+longest :: Seq.Seq Cs -> Seq.Seq Cs
+longest bs = seqFoldl f (Seq.singleton Seq.empty) bs where
+  f acc x | Seq.length x > Seq.length (seqHead acc) = Seq.singleton x
+          | Seq.length x == Seq.length (seqHead acc) = x Seq.<| acc
+          | otherwise = acc
 
 run :: IO ()
 run = do
