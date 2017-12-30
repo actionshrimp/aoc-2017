d47f75c53a2507cd5e17e7c3b4f36ff131193140
Dave Aitken <dave.aitken@gmail.com>
2017-12-30 12:39:53 +0000

    Day 24 part 1 in a reasonable time! (5s)

--- src/Sol24.hs
+++ src/Sol24.hs
@@ -5,24 +5,30 @@ module Sol24
 
 import Data.List.Split (splitOn)
 import Data.List (delete, nub, sort)
+import Data.Foldable (foldl')
 
-type Components = [(Int, Int)]
+--Components
+type Cs = [(Int, Int)]
 
-parseInput :: String -> Components
+parseInput :: String -> Cs
 parseInput s = [(read a, read b) | l  <- lines s, let [a, b] = splitOn "/" l ]
 
-toFixedPoint :: Eq a => (a -> a) -> a -> a
-toFixedPoint f x = let y = f x in
-  if y == x then x else toFixedPoint f y
+expandOne :: (Cs, Cs) -> Maybe [(Cs, Cs)]
+expandOne (available, current@((_, cp):cs)) =
+  case filter (\(apa, apb) -> apa == cp || apb == cp) available of
+    [] -> Nothing
+    matches -> Just $ map (\a@(apa, apb) -> (delete a available, if apa == cp then a:current else (apb, apa):current)) matches
 
-expandValid :: [(Components, Components)] -> [(Components, Components)]
-expandValid xs = nub $ xs ++ do
-  x@(cs, b@((_, bp):bs)) <- xs
-  c@(cpa, cpb) <- filter (\(cpa, cpb) -> cpa == bp || cpb == bp) cs
-  return $ (delete c cs, if cpa == bp then c:b else (cpb, cpa):b)
+expandMany :: [(Cs, Cs)] -> [(Cs, Cs)]
+expandMany work = foldl' f [] work where
+  f moreWork w = case expandOne w of
+    Nothing -> moreWork
+    Just exps -> exps ++ moreWork
 
-validBridges :: Components -> [Components]
-validBridges cs = delete [] $ map (init . snd) $ toFixedPoint expandValid [(cs, [(0, 0)])]
+validBridges' :: Cs -> [Cs]
+validBridges' cs = delete [] $ go [(cs, [(0, 0)])] where
+  go [] = []
+  go work = map (init . snd) work ++ go (expandMany work)
 
 exampleComponentsRaw = "\
 \0/2\n\
@@ -34,19 +40,21 @@ exampleComponentsRaw = "\
 \10/1\n\
 \9/10"
 
-strength :: Components -> Int
+strength :: Cs -> Int
 strength cs = sum $ map (\(a, b) -> a+b) cs
+
+strongest :: [Cs] -> Int
+strongest bs = head . reverse . sort $ map strength bs
 
 run :: IO ()
 run = do
   inputRaw <- readFile "data/24.txt"
   putStrLn "example: "
-  let exampleBridges = (validBridges (parseInput exampleComponentsRaw))
-  mapM_ (putStrLn . show) exampleBridges
-  putStrLn $ "max strength: " ++ (show (head . reverse . sort $ map strength exampleBridges))
+  let exampleBridges = (validBridges' (parseInput exampleComponentsRaw))
+  -- mapM_ (putStrLn . show) exampleBridges
+  putStrLn $ "max strength: " ++ (show (strongest exampleBridges))
   putStrLn ""
   putStrLn "part1: "
-  putStrLn inputRaw
-  let p1Bridges = (validBridges (parseInput inputRaw))
+  let p1Bridges = (validBridges' (parseInput inputRaw))
   -- mapM_ (putStrLn . show) p1Bridges
-  putStrLn $ "max strength: " ++ (show (head . reverse . sort $ map strength p1Bridges))
+  putStrLn $ "max strength: " ++ (show (strongest p1Bridges))
